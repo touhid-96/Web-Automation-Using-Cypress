8398dae97c253d6c305ddf32f69069fb5020c2f4
touhid-96 <touhidul.islam.official@gmail.com>
2024-10-28 23:17:54 +0600

    added automation video output

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress")
 module.exports = defineConfig({
 	watchForFileChanges: false,
 	reporter: "cypress-mochawesome-reporter",
+	video: true,
 	e2e: {
 		setupNodeEvents(on, config) {
 			require("cypress-mochawesome-reporter/plugin")(on)
